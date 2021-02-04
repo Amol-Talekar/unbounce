4fd0352329bc04e6098d3263297a275e5a45af30
Kamal Gupta <Kamaldro97@gmail.com>
2021-02-04 13:14:30 +0530

    images & test changing

--- script.js
+++ script.js
@@ -14,17 +14,25 @@ function topbar(){
       
     }
 }
+var images =[
+    "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-sales_sm.jpg')",
+    "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-customers_sm.jpg')",
+    "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-leads_sm.jpg')"
+]
 function changeBackground(){
-    document.querySelector('.header').style.backgroundImage = "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-sales_sm.jpg')";
-    // if(document.querySelector('.header').style.backgroundImage == "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-sales_sm.jpg')"){
-    //     document.querySelector('.header').style.backgroundImage = "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-customers_sm.jpg')";
-    // }
-    // else if(document.querySelector('.header').style.backgroundImage = "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-customers_sm.jpg')"){
-    //     document.querySelector('.header').style.backgroundImage = "url('https://mk0zofoqaluvgdskgvsb.kinstacdn.com/photos/landing-page-convert-more-leads_sm.jpg')";  
-    // }
+    document.querySelector('.header').style.backgroundImage =images[0]
+    document.querySelector('.inside_cont_text > p').innerHTML ="Convert More Sales"
 }
-
-setInterval(changeBackground,1000)
-
+function changeBackground_1(){
+    document.querySelector('.header').style.backgroundImage =images[1]
+    document.querySelector('.inside_cont_text > p').innerHTML ="Convert More Customers"
+}
+function changeBackground_2(){
+    document.querySelector('.header').style.backgroundImage =images[2]
+    document.querySelector('.inside_cont_text > p').innerHTML ="Convert More leads"
+}
+setInterval(changeBackground,2000)
+setInterval(changeBackground_1,4000)
+setInterval(changeBackground_2,6000)
 
 document.addEventListener('scroll',topbar)
